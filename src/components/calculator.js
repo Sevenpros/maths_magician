89fae468e5683acd7b26d290e7c002e40a6dd788
Sevenpros <sethbisoso@gmail.com>
2022-08-05 17:22:36 +0200

    Refactor with Hooks

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,67 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../calc.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor() {
-    super();
-    this.MapCalculator = this.MapCalculator.bind(this);
-    this.OnClickListening = this.OnClickListening.bind(this);
-    this.state = {
-      total: '',
-      next: '',
-      operation: '',
-    };
-  }
+const Calculator = () => {
+  const [num, setNum] = useState({ total: '', next: '', operation: '' });
+  const OnClickListening = (e) => {
+    setNum(calculate(num, e.target.innerText));
+  };
 
-  OnClickListening(e) {
-    const number = e.target.innerText;
-    const results = calculate(this.state, number);
-    this.setState(() => ({
-      next: results.next,
-      total: results.total,
-      operation: results.operation,
-    }));
-  }
-
-  MapCalculator() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calc-form">
-        <div className="first-row" id="first-row">
-          <input
-            type="text"
-            readOnly
-            placeholder="0"
-            value={(total || '') + (operation || '') + (next || '') || 0}
-          />
-        </div>
-        <button type="button" className="number" onClick={this.OnClickListening}>AC</button>
-        <button type="button" className="number" onClick={this.OnClickListening}>+/-</button>
-        <button type="button" className="number" onClick={this.OnClickListening}>%</button>
-        <button type="button" className="operation" onClick={this.OnClickListening}>÷</button>
-        <button type="button" className="number" onClick={this.OnClickListening}>7</button>
-        <button type="button" className="number" onClick={this.OnClickListening}>8</button>
-        <button type="button" className="number" onClick={this.OnClickListening}>9</button>
-        <button type="button" className="operation" onClick={this.OnClickListening}>x</button>
-        <button type="button" className="number" onClick={this.OnClickListening}>4</button>
-        <button type="button" className="number" onClick={this.OnClickListening}>5</button>
-        <button type="button" className="number" onClick={this.OnClickListening}>6</button>
-        <button type="button" className="operation" onClick={this.OnClickListening}>-</button>
-        <button type="button" className="number" onClick={this.OnClickListening}>1</button>
-        <button type="button" className="number" onClick={this.OnClickListening}>2</button>
-        <button type="button" className="number" onClick={this.OnClickListening}>3</button>
-        <button type="button" className="operation" onClick={this.OnClickListening}>+</button>
-        <button type="button" className="number last-row" onClick={this.OnClickListening}>0</button>
-        <button type="button" className="number" onClick={this.OnClickListening}>.</button>
-        <button type="button" className="operation" onClick={this.OnClickListening}>=</button>
+  const { total, next, operation } = num;
+  return (
+    <div className="calc-form">
+      <div className="first-row" id="first-row">
+        <input
+          type="text"
+          readOnly
+          placeholder="0"
+          value={(total || '') + (operation || '') + (next || '') || 0}
+        />
       </div>
-    );
-  }
+      <button type="button" className="number" onClick={OnClickListening}>AC</button>
+      <button type="button" className="number" onClick={OnClickListening}>+/-</button>
+      <button type="button" className="number" onClick={OnClickListening}>%</button>
+      <button type="button" className="operation" onClick={OnClickListening}>÷</button>
+      <button type="button" className="number" onClick={OnClickListening}>7</button>
+      <button type="button" className="number" onClick={OnClickListening}>8</button>
+      <button type="button" className="number" onClick={OnClickListening}>9</button>
+      <button type="button" className="operation" onClick={OnClickListening}>x</button>
+      <button type="button" className="number" onClick={OnClickListening}>4</button>
+      <button type="button" className="number" onClick={OnClickListening}>5</button>
+      <button type="button" className="number" onClick={OnClickListening}>6</button>
+      <button type="button" className="operation" onClick={OnClickListening}>-</button>
+      <button type="button" className="number" onClick={OnClickListening}>1</button>
+      <button type="button" className="number" onClick={OnClickListening}>2</button>
+      <button type="button" className="number" onClick={OnClickListening}>3</button>
+      <button type="button" className="operation" onClick={OnClickListening}>+</button>
+      <button type="button" className="number last-row" onClick={OnClickListening}>0</button>
+      <button type="button" className="number" onClick={OnClickListening}>.</button>
+      <button type="button" className="operation" onClick={OnClickListening}>=</button>
+    </div>
+  );
+};
 
-  render() {
-    return <this.MapCalculator />;
-  }
-}
 Calculator.defaultProps = { total: 0, operations: '', next: 0 };
 export default Calculator;
