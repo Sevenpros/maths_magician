8e2aa954e93f6345880ef8dc2c8ac7cf16b35c6f
Sevenpros <sethbisoso@gmail.com>
2022-08-12 13:36:19 +0200

    Add full website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/calculator';
+import Home from './pages/Home';
+import Quotes from './pages/quote';
 
 function App() {
   return (
-    <Calculator />
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quotes" element={<Quotes />} />
+      </Routes>
+    </>
   );
 }
 
